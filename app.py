089edb073b4fee5ab558ef286f2369472ed2729d
leerobertdyer <leerobertdyer@gmail.com>
2023-11-07 11:09:22 -0500

    added card and started api

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+myPokemons = []
+
+@app.route('/', methods=['GET', 'POST'])
 def pokemonForm():
-    return render_template('pokemonForm.html')
+    if request.method == 'POST':
+        pokemonName = request.form['pokemonName'].lower()
+        r = requests.get('https://pokeapi.co/api/v2/pokemon/')
+        data = r.json()['results']
+        pokemonUrl = ''
+        for pokemon in data:
+            if pokemon["name"] == pokemonName:
+                pokemonUrl = pokemon['url']
+                break
+        if pokemonUrl:
+            stats = requests.get(pokemonUrl).json()
+            return render_template('card.html', pokemonName=pokemonName, stats=stats)
+        else:
+            return render_template('pokemonForm.html')
+    else:
+        return render_template('pokemonForm.html')
 
 @app.route('/card')
 def card():
-    return render_template('card.html')
+    pokemonName = "Charizar"
+    return render_template('card.html', pokemonName=pokemonName)
+
 @app.route('/holyshit')
 def holyShit():
     return render_template('holyshit.html')
