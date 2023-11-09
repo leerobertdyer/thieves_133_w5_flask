71cebf64d2bb349226fca68a0242920df577ef7b
leerobertdyer <leerobertdyer@gmail.com>
2023-11-08 22:45:53 -0500

    added forms and styles

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,15 @@
-from flask import request, render_template
+from flask import request, render_template,redirect, url_for
+
 import requests
 from app import app
-from .forms import selectPokemon
+from .forms import selectPokemon, SignupForm, LoginForm
 
 
 myPokemons = []
+REGISTERED_USERS = [{'email': 'lee@leedyer.com', 'password': 'lee'}]
 
 @app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def pokemonForm():
     form = selectPokemon()
     if request.method == 'POST':
@@ -38,3 +41,39 @@ def holyShit():
     else:
         return render_template('pokemonForm.html')
 
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    form = SignupForm()
+    if request.method == "POST" and form.validate_on_submit():
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        password = request.form['password']
+        for user in REGISTERED_USERS:
+            print(user)
+            # if user['email'] == email:
+            #     return "User already registered, please login" #change to an error.html with nav
+        REGISTERED_USERS.append({(first_name + ' ' + last_name):{'email': email, 'password': password}})
+        print(REGISTERED_USERS)
+        return render_template('profile.html')
+        
+    else:
+        return render_template('signup.html', form=form)
+    
+@app.route('/profile', methods=["GET"])
+def profile():
+    return render_template('profile.html')
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if request.method == "POST" and form.validate_on_submit():
+        email = request.form['email']
+        password = request.form['password']
+        for user in REGISTERED_USERS:
+            if user['email'] == email:
+                if user['password'] == password:
+                    return render_template('profile.html')
+        return render_template('holyShit.html')
+    else:
+        return render_template('login.html', form=form)
